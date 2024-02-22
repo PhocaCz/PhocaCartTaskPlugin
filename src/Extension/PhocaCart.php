3c222b57e7ace78ed000fa9a619485113a407e07
Jan <PhocaCz@users.noreply.github.com>
2024-02-22 23:27:41 +0100

    Initial commit

---
+++ src/Extension/PhocaCart.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package   Phoca Cart
+ * @author    Jan Pavelka - https://www.phoca.cz
+ * @copyright Copyright (C) Jan Pavelka https://www.phoca.cz
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 and later
+ * @cms       Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
+
+namespace Joomla\Plugin\Task\PhocaCart\Extension;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Menu\AdministratorMenuItem;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Uri\Uri;
+use Phoca\PhocaCart\User\AdvancedACL;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * PhocaCart Task plugin
+ *
+ * @since  5.0.0
+ */
+final class PhocaCart extends CMSPlugin
+{
+
+}
